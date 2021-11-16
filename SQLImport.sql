3f7eb69deb2e080d9c037b4a4b73dc5a71ca099f
PGL50 <84110521+PGL50@users.noreply.github.com>
2021-11-15 19:19:47 -0600

    README

--- SQLImport.sql
+++ SQLImport.sql
@@ -1,5 +1,6 @@
 --drop table red_wine ;
 
+-- Create table with the outcome (quality) and 11 features for red wine
 CREATE TABLE red_wine (
      fixed_acidity float ,
 	     volatile_acidity float ,
@@ -15,13 +16,17 @@ CREATE TABLE red_wine (
 		 quality float
 );
 
+-- Import csv file
+
 select * from red_wine ;
 
+-- Add ID column and color column = 'red'
 ALTER TABLE red_wine ADD COLUMN ID SERIAL PRIMARY KEY;
 ALTER TABLE red_wine ADD color varchar(5) DEFAULT 'red' ;
 
 --drop table white_wine ; 
 
+-- Create table with the outcome (quality) and 11 features for red wine
 CREATE TABLE white_wine (
      fixed_acidity float ,
 	     volatile_acidity float ,
@@ -37,15 +42,18 @@ CREATE TABLE white_wine (
 		 quality float
 );
 
+-- Import csv file
+
 select * from white_wine ;
 
+-- Add ID column and color column = 'white'
 ALTER TABLE white_wine ADD COLUMN ID SERIAL PRIMARY KEY;
 ALTER TABLE white_wine ADD color varchar(5) DEFAULT 'white' ;
 
+-- Update white wine ID so when the tables are merged together the ID's are unique
 UPDATE white_wine SET ID = ID+20000 ;
 
+--Create new tables to bring in all wine data
 CREATE TABLE all_wines AS (SELECT * FROM red_wine);
 INSERT INTO all_wines SELECT * FROM white_wine;
 
-
-INSERT INTO red_wine SELECT * FROM white_wine ;
